35d61e18bb51152065f30401a6f062a165d84ad2
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-05-15 17:43:54 -0300

    Add PersistanceConfiguration class

---
+++ TABP.Infrastructure/Persistence/PersistenceConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TABP.Infrastructure.Persistance;
+
+public static class PersistenceConfiguration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<TabpDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("SqlServerConnection")));
+
+        return services;
+    }
+}
