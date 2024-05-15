87d8706045d095bf1f31b954f48dc292ab7805f5
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-05-15 15:43:44 -0300

    Add Amenity Entity

---
+++ TABP.Domain/Entities/Amenity.cs
@@ -0,0 +1,9 @@
+﻿namespace TABP.Domain.Entities;
+
+public class Amenity
+{
+    public Guid AmenityId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Hotel> Hotels { get; set; }
+}
