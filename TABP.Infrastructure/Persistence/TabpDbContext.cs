466b8619592540557fdb27987a7b9067bae87e06
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-05-15 17:43:24 -0300

    Add TabpDbContext

---
+++ TABP.Infrastructure/Persistence/TabpDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TABP.Domain.Entities;
+
+namespace TABP.Infrastructure.Persistance;
+
+public class TabpDbContext : DbContext
+{
+    public TabpDbContext(DbContextOptions<TabpDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Amenity> Amenities { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<HotelDiscount> HotelDiscounts { get; set; }
+    public DbSet<HotelImage> HotelImages { get; set; }
+    public DbSet<Owner> Owners { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<RoomDiscount> RoomDiscounts { get; set; }
+    public DbSet<RoomImage> RoomImages { get; set; }
+    public DbSet<RoomType> RoomTypes { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Booking>()
+            .Property(b => b.PriceAtBooking)
+            .HasColumnType("decimal(18,2)");
+
+        modelBuilder.Entity<Payment>()
+            .Property(p => p.Amount)
+            .HasColumnType("decimal(18,2)");
+
+        modelBuilder.Entity<Room>()
+            .Property(r => r.Price)
+            .HasColumnType("decimal(18,2)");
+    }
+    
+    
+}
