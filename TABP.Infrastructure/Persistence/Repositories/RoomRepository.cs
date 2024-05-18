669f491d0272d3555de1fb68c09015f79d525ecd
FranciscoWatson <francisco@watson.com.ar>
2024-05-18 14:50:57 -0300

    Add RoomRepository Implementation

---
+++ TABP.Infrastructure/Persistence/Repositories/RoomRepository.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using TABP.Application.Interfaces.Repositories;
+using TABP.Domain.Entities;
+
+namespace TABP.Infrastructure.Persistence.Repositories;
+
+public class RoomRepository : IRoomRepository
+{
+    private readonly TabpDbContext _context;
+
+    public RoomRepository(TabpDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Room?> GetByIdAsync(Guid id)
+    {
+        return await _context.Rooms.FindAsync(id);
+    }
+
+    public async Task<List<Room>> GetAllAsync()
+    {
+        return await _context.Rooms.ToListAsync();
+    }
+
+    public async Task<Room> CreateAsync(Room room)
+    {
+        await _context.Rooms.AddAsync(room);
+        await _context.SaveChangesAsync();
+        return room;
+    }
+
+    public async Task UpdateAsync(Room room)
+    {
+        _context.Rooms.Update(room);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var room = new Room { RoomId = id };
+        _context.Rooms.Attach(room);
+        _context.Rooms.Remove(room);
+        await _context.SaveChangesAsync();
+    }
+}
