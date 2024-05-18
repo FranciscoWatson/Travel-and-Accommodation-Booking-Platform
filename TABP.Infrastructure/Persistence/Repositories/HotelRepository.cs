5067caec9f4dd324041c04f626305cff4197c58f
FranciscoWatson <francisco@watson.com.ar>
2024-05-18 14:44:55 -0300

    Add HotelRepository Implementation

---
+++ TABP.Infrastructure/Persistence/Repositories/HotelRepository.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using TABP.Application.Interfaces.Repositories;
+using TABP.Domain.Entities;
+
+namespace TABP.Infrastructure.Persistence.Repositories;
+
+public class HotelRepository : IHotelRepository
+{
+    private readonly TabpDbContext _context;
+
+    public HotelRepository(TabpDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Hotel?> GetByIdAsync(Guid id)
+    {
+        return await _context.Hotels.FindAsync(id);
+    }
+
+    public async Task<List<Hotel>> GetAllAsync()
+    {
+        return await _context.Hotels.ToListAsync();
+    }
+
+    public async Task<Hotel> CreateAsync(Hotel hotel)
+    {
+        await _context.Hotels.AddAsync(hotel);
+        await _context.SaveChangesAsync();
+        return hotel;
+    }
+
+    public async Task UpdateAsync(Hotel hotel)
+    {
+        _context.Hotels.Update(hotel);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var hotel = new Hotel { HotelId = id };
+        _context.Hotels.Attach(hotel);
+        _context.Hotels.Remove(hotel);
+        await _context.SaveChangesAsync();
+    }
+}
