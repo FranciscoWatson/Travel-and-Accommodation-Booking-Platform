6c673567bba09fe8c2aa41b97b10bb7e2cfc53ef
FranciscoWatson <francisco@watson.com.ar>
2024-05-18 15:15:16 -0300

    Add Repositories Dependencty Injection

--- TABP.Infrastructure/Persistence/PersistenceConfiguration.cs
+++ TABP.Infrastructure/Persistence/PersistenceConfiguration.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TABP.Application.Interfaces.Repositories;
+using TABP.Infrastructure.Persistence.Repositories;
 
 namespace TABP.Infrastructure.Persistence;
 
@@ -11,6 +13,28 @@ public static class PersistenceConfiguration
         services.AddDbContext<TabpDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("SqlServerConnection")));
 
+        services.AddRepositories();
+        
+        return services;
+    }
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IAmenityRepository , AmenityRepository>();
+        services.AddScoped<IBookingRepository , BookingRepository>();
+        services.AddScoped<ICityRepository , CityRepository>();
+        services.AddScoped<ICountryRepository , CountryRepository>();
+        services.AddScoped<IHotelImageRepository, HotelImageRepository>();
+        services.AddScoped<IHotelRepository , HotelRepository>();
+        services.AddScoped<IOwnerRepository , OwnerRepository>();
+        services.AddScoped<IPaymentRepository , PaymentRepository>();
+        services.AddScoped<IReviewRepository , ReviewRepository>();
+        services.AddScoped<IRoomDiscountRepository , RoomDiscountRepository>();
+        services.AddScoped<IRoomImageRepository , RoomImageRepository>();
+        services.AddScoped<IRoomRepository , RoomRepository>();
+        services.AddScoped<IRoomTypeRepository , RoomTypeRepository>();
+        services.AddScoped<IUserRepository , UserRepository>();
+        services.AddScoped<IUserRoleRepository , UserRoleRepository>();
+        
         return services;
     }
 }
