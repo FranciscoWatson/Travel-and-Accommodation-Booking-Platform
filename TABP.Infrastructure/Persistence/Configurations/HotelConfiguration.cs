d55f09d62ab344f2cf1e6aa9ad3c44f24fa248c0
FranciscoWatson <francisco@watson.com.ar>
2024-05-20 12:06:23 -0300

    Add Migration to Change StarRating to Float

--- TABP.Infrastructure/Persistence/Configurations/HotelConfiguration.cs
+++ TABP.Infrastructure/Persistence/Configurations/HotelConfiguration.cs
@@ -16,7 +16,6 @@ public class HotelConfiguration : IEntityTypeConfiguration<Hotel>
         builder.Property(h => h.Description).HasMaxLength(500);
         builder.Property(h => h.PhoneNumber).HasMaxLength(15);
         builder.Property(h => h.Email).HasMaxLength(100);
-        builder.Property(h => h.StarRating).HasMaxLength(5);
         builder.Property(h => h.Address).HasMaxLength(300);
         builder.Property(h => h.Latitude).HasMaxLength(50);
         builder.Property(h => h.Longitude).HasMaxLength(50);
