11f3eb93c6d7657f0a9d2feb363784f78ff14deb
FranciscoWatson <francisco@watson.com.ar>
2024-05-20 11:58:02 -0300

    Add SearchHotelQuery

---
+++ TABP.Application/Queries/Hotels/SearchHotelQuery.cs
@@ -0,0 +1,18 @@
+using MediatR;
+using TABP.Application.DTOs.HotelDTOs;
+
+namespace TABP.Application.Queries.Hotels;
+
+public class SearchHotelQuery : IRequest<List<HotelSearchResponseDto>>
+{
+    public DateTime CheckIn { get; set; }
+    public DateTime CheckOut { get; set; }
+    public int NumberOfAdults { get; set; }
+    public int NumberOfChildren { get; set; }
+    public int NumberOfRooms { get; set; }
+    public string? RoomType { get; set; }
+    public string? HotelName { get; set; }
+    public string? City { get; set; }
+    public decimal? MaxPrice { get; init; }
+    public int? MinRating { get; init; }
+}
