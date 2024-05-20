6b3db77603e4cfdc297927a81aed567c5187d3fd
FranciscoWatson <francisco@watson.com.ar>
2024-05-20 13:15:40 -0300

    Add HotelsController

---
+++ TABP.API/Controllers/HotelsController.cs
@@ -0,0 +1,33 @@
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TABP.Application.DTOs.HotelDTOs;
+using TABP.Application.Queries.Hotels;
+
+namespace Travel_and_Accommodation_Booking_Platform.Controllers;
+
+[ApiController]
+[Route("/api/hotels")]
+public class HotelsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+
+    public HotelsController(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<HotelSearchResponseDto>>> SearchHotels(
+        [FromQuery] SearchHotelRequestDto searchHotelRequest,
+        CancellationToken cancellationToken = default)
+    {
+        var query = _mapper.Map<SearchHotelQuery>(searchHotelRequest);
+        
+        var hotels = await _mediator.Send(query, cancellationToken);
+        
+        return Ok(hotels);
+    }
+}
