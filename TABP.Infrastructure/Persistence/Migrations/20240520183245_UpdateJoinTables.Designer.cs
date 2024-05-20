808dc62574e0c26e90de49b148ffc2c942533e65
FranciscoWatson <francisco@watson.com.ar>
2024-05-20 15:43:03 -0300

    Add Migration to UpdateJoinTables

--- TABP.Infrastructure/Persistence/Migrations/20240520183245_UpdateJoinTables.Designer.cs
+++ TABP.Infrastructure/Persistence/Migrations/20240520183245_UpdateJoinTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TABP.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using TABP.Infrastructure.Persistence;
 namespace TABP.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(TabpDbContext))]
-    partial class TabpDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240520183245_UpdateJoinTables")]
+    partial class UpdateJoinTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
