1868a3e578f2011462414feb1f861159ce68e043
FranciscoWatson <francisco@watson.com.ar>
2024-05-29 12:53:28 -0300

    Add PostalCode to City Entity

--- TABP.Domain/Entities/City.cs
+++ TABP.Domain/Entities/City.cs
@@ -1,11 +1,12 @@
 ﻿namespace TABP.Domain.Entities;
 
-public class City
+public class City : AuditableEntity
 {
     public Guid CityId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public string? ThumbnailImage { get; set; }
+    public string PostalCode { get; set; }
     public Guid CountryId { get; set; }
     public Country Country { get; set; }
     public List<Hotel> Hotels { get; set; }
--- TABP.Infrastructure/Persistence/Configurations/CityConfiguration.cs
+++ TABP.Infrastructure/Persistence/Configurations/CityConfiguration.cs
@@ -14,5 +14,6 @@ public class CityConfiguration : IEntityTypeConfiguration<City>
         builder.Property(c => c.Name).HasMaxLength(100);
         builder.Property(c => c.Description).HasMaxLength(300);
         builder.Property(c => c.ThumbnailImage).HasMaxLength(500);
+        builder.Property(c => c.PostalCode).HasMaxLength(15);
     }
 }
