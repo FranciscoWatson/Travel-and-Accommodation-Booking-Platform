1f57fc15298f61e17421ddf8cc0bf7d3a6e58a01
FranciscoWatson <francisco@watson.com.ar>
2024-06-05 22:43:35 -0300

    Add Documentation to User Controller

--- TABP.API/Controllers/UsersController.cs
+++ TABP.API/Controllers/UsersController.cs
@@ -21,7 +21,23 @@ public class UsersController : ControllerBase
         _mediator = mediator;
         _mapper = mapper;
     }
-    
+
+    /// <summary>
+    /// Retrieves a list of hotels recently visited by a specific user.
+    /// </summary>
+    /// <param name="userId">The ID of the user whose hotel visit history is being requested.</param>
+    /// <param name="recentlyVisitedHotelRequestDto">Parameters to specify the number of recently visited hotels to retrieve.</param>
+    /// <param name="cancellationToken"></param>
+    /// <response code="200">Returns a list of recently visited hotels if available.</response>
+    /// <response code="400">If the input parameters are invalid.</response>
+    /// <response code="404">If no recent visits are found or the user does not exist.</response>
+    /// <response code="401">If the user is not authenticated.</response>
+    /// <response code="403">If the user is not authorized to view the hotel visit history.</response>
+    [ProducesResponseType(typeof(IEnumerable<RecentlyVisitedHotelResponseDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
     [HttpGet("{userId}/recently-visited-hotels")]
     [Authorize(Roles = "Guest", Policy = "MatchUserId")]
     public async Task<ActionResult<IEnumerable<RecentlyVisitedHotelResponseDto>>> GetRecentlyVisitedHotels(
